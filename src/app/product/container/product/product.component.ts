a87e74e9b42febacf396d9e0d9b91f6d97ea2efc
Radhakrishnan Gopal <Radhakrishna.Gopal@dowjones.com>
2021-05-27 15:52:12 +0530

    add error handler

--- src/app/product/container/product/product.component.ts
+++ src/app/product/container/product/product.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../../services/product.service';
 
+import { Product } from '../../models/product';
+import { ProductError } from '../../models/product-error';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+
+  productList: Product[] | undefined;
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getAllProduct().subscribe((data) => console.log(data));
+    this.productService.getAllProduct().subscribe((data: Product[] | ProductError) => {
+       this.productList = <Product[]>data;
+    }, (err:ProductError) => console.log(err));
   }
 }
